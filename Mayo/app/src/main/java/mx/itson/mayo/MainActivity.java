48a7061c68c24b1d7151dd6287222bcf1f2f1082
Jaime Escobar <jaime.escobar.mtz@gmail.com>
2019-04-30 15:57:11 -0700

    Layout update

--- Mayo/app/src/main/java/mx/itson/mayo/MainActivity.java
+++ Mayo/app/src/main/java/mx/itson/mayo/MainActivity.java
@@ -2,6 +2,12 @@ package mx.itson.mayo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import mx.itson.mayo.database.MayoDB;
+import mx.itson.mayo.entidades.Usuario;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new MayoDB(getApplicationContext(), "MayoDB", null, 1);
+        final EditText etNombre = findViewById(R.id.etNombre);
+        final EditText etCorreo = findViewById(R.id.etCorreo);
+        final EditText etTelefono = findViewById(R.id.etTelefono);
+        findViewById(R.id.btnGuardar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!etNombre.getText().toString().isEmpty() &&
+                        !etCorreo.getText().toString().isEmpty() &&
+                        !etTelefono.getText().toString().isEmpty()){
+                    Usuario u = new Usuario(getApplicationContext());
+                    u.guardar(etNombre.getText().toString(),etTelefono.getText().toString(), etCorreo.getText().toString());
+                }else{
+                    Toast.makeText(getApplicationContext(), "Faltan datos", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
